b438432925abb233b246206cb74038d565ff8219
H34th3rX <heatherpretty1@gmail.com>
2025-11-25 13:57:27 -0400

    Initial Commit - Proyecto algoritmos paralelos

---
+++ test.py
@@ -0,0 +1,11 @@
+import tkinter
+import psutil
+import matplotlib
+import threading
+
+print("✅ Todas las librerías instaladas correctamente!")
+print(f"Python: OK")
+print(f"Tkinter: OK")
+print(f"Psutil: OK")
+print(f"Matplotlib: OK")
+print(f"Threading: OK")
